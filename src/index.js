232167bb507fcc7644f6fca18c6aa57ac0bb8733
Apolo Moraes <vlogsapolo@gmail.com>
2023-02-23 13:58:40 -0300

    validando cpf existente

--- src/index.js
+++ src/index.js
@@ -9,12 +9,18 @@ const costumers = [];
 app.post("/account", (req, res) => {
   const { cpf, name } = req.body;
 
+  const customerAlreadyExists = costumers.some((customer) => customer.cpf === cpf);
+
+  if (customerAlreadyExists) {
+    return res.status(400).json({ error: "Customer already exists!" })
+  }
+
   const id = uuidv4();
 
   costumers.push({
     cpf,
     name,
-    id,
+    id: uuidv4(),
     statement: []
   });
 
