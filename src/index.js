d4829456be88f8da0da3cad5b6f9478b7873ae02
Apolo Moraes <vlogsapolo@gmail.com>
2023-02-23 11:56:02 -0300

    cadastro de conta

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 const express = require('express')
+const { v4: uuidv4 } = require('uuid')
 
-const app = express()
+const app = express();
+app.use(express.json());
 
+const costumers = [];
 
+app.post("/account", (req, res) => {
+  const { cpf, name } = req.body;
+
+  const id = uuidv4();
+
+  costumers.push({
+    cpf,
+    name,
+    id,
+    statement: []
+  });
+
+  return res.status(201).end();
+});
 
 app.listen(3333);
