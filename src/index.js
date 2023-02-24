f08fac60afd3af1052d6d8df704ec73d9fab71da
Apolo Moraes <vlogsapolo@gmail.com>
2023-02-24 12:27:01 -0300

    add rota deletar usuário e rota para fazer balanço dos valores

--- src/index.js
+++ src/index.js
@@ -130,6 +130,20 @@ app.get("/account", verifyIfExistAccountCPF, (req, res) => {
   return res.json(customer);
 })
 
+app.delete("/account", verifyIfExistAccountCPF, (req, res) => {
+  const { customer } = req;
 
+  customers.splice(customer, 1);
+
+  return res.status(200).json(customers)
+})
+
+app.get("/balance", verifyIfExistAccountCPF, (req, res) => {
+  const { customer } = req;
+
+  const balance = getBalance(customer.statement);
+
+  return res.json(balance);
+})
 
 app.listen(3333);
