d9c96ffcbb5a5a51f6917f03d39165bc06538b5a
Apolo Moraes <vlogsapolo@gmail.com>
2023-02-24 11:48:57 -0300

    listar extrato bancario

--- src/index.js
+++ src/index.js
@@ -98,4 +98,15 @@ app.post("/withdraw", verifyIfExistAccountCPF, (req, res) => {
   return res.status(201).send();
 })
 
+app.get("/statement/date", verifyIfExistAccountCPF, (req, res) => {
+  const { customer } = req;
+  const { date } = req.query;
+
+  const dateFormat = new Date(date + " 00:00");
+
+  const statement = customer.statement.filter((statement) => statement.created_at.toDateString() === new Date(dateFormat).toDateString());
+
+  return res.json(statement);
+});
+
 app.listen(3333);
